7558a84c89a57367f2cb8afa4ed9104ea51f2d4c
Tapio Mäntysalo <tapio.mantysalo@live.com>
2021-02-20 18:26:17 +0200

    first commit

---
+++ demo.py
@@ -0,0 +1,40 @@
+# Note: requires  pip install matplotlib==3.2.2 due to bug in backtrader
+from datetime import datetime
+import backtrader as bt
+
+# Create a subclass of Strategy to define the indicators and logic
+
+
+class SmaCross(bt.Strategy):
+    # list of parameters which are configurable for the strategy
+    params = dict(
+        pfast=12,  # period for the fast moving average
+        pslow=26   # period for the slow moving average
+    )
+
+    def __init__(self):
+        sma1 = bt.ind.SMA(period=self.p.pfast)  # fast moving average
+        sma2 = bt.ind.SMA(period=self.p.pslow)  # slow moving average
+        self.crossover = bt.ind.CrossOver(sma1, sma2)  # crossover signal
+
+    def next(self):
+        if not self.position:  # not in the market
+            if self.crossover > 0:  # if fast crosses slow to the upside
+                self.buy()  # enter long
+
+        elif self.crossover < 0:  # in the market & cross to the downside
+            self.close()  # close long position
+
+
+cerebro = bt.Cerebro()  # create a "Cerebro" engine instance
+
+# Create a data feed
+data = bt.feeds.YahooFinanceData(dataname='INTC',
+                                 fromdate=datetime(2017, 1, 1),
+                                 todate=datetime(2021, 12, 31))
+
+cerebro.adddata(data)  # Add the data feed
+
+cerebro.addstrategy(SmaCross)  # Add the trading strategy
+cerebro.run()  # run it all
+cerebro.plot()  # and plot it with a single command
